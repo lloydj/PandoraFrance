5903653a260a66a3dcdd596bbae071e75dd40e18
SuLloyd <sulloyd@MacBook-Pro-de-Lloyd.local>
2018-10-25 13:52:30 -0300

    Netlify bug test4

--- src/pages/pointsdeventes.js
+++ src/pages/pointsdeventes.js
@@ -5,7 +5,6 @@ import Layout from '../components/layouts'
 import BannerLanding from '../components/BannerLanding'
 
 import pic08 from '../assets/images/Phone1.png'
-import pic09 from '../assets/images/Iphone02.png'
 import pic10 from '../assets/images/stats.png'
  
 const Landing = (props) => (
